cd58c27a198b8234e5e171901dc736009f44d2eb
Nattawat lertchidomrongsri <little0747@gmail.com>
2025-12-05 22:12:26 +0700

    Add file C

---
+++ 4.8 quiz game.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+   setbuf(stdout, NULL);
+
+   // QUIZ GAME
+
+   char questions[][100] = {"What is the largest planet in the solar system?",
+                            "What is the hottest planet?",
+                            "What planet has the most moon?",
+                            "Is the Earth flat?"};
+
+   char options[][100] = {"A. Jupiter\nB. Saturn\nC. Uranus\nD. Neptune",
+                          "A. Mercury\nB. Venus\nC. Earth\nD. Mars",
+                          "A. Earth\nB. Mars\nC. Jupiter\nD. saturn",
+                          "A. Yes\nB. No\nC. Maybe\nD. Sometimes"};
+
+   char answerKey[] = {'A', 'B', 'D', 'B'};
+
+   printf("%s", questions[0]);
+   printf("%s", options[0]);
+   printf("%c", answerKey[0])
+
+   //Ex.1
+
+   char guess = '\0';
+
+   printf("*** QUIZ GAME ***\n");
+
+   for(int i = 0; i < 4; i++){
+      printf("\n%s\n", questions[i]);
+      printf("\n%s\n", options[i]);
+      printf("Enter your choice: ");
+      scanf(" %c", &guess);
+
+   }
+
+   return 0;
+}
